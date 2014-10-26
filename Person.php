39e21fa4b5c5b8fb284b018216484c6b15ddc1f8
kennyhack3 <kennyhack3@seznam.cz>
2014-10-26 22:48:44 +0100

    classes

--- Person.php
+++ Person.php
@@ -4,4 +4,7 @@
  * User: kennyhack
  * Date: 26.10.2014
  * Time: 22:28
- */ 
+ */
+class Person {
+
+}
--- Zoo.php
+++ Zoo.php
@@ -4,4 +4,7 @@
  * User: kennyhack
  * Date: 26.10.2014
  * Time: 22:22
- */ 
+ */
+class Zoo {
+
+}
